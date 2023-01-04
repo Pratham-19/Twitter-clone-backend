ae80ca771d35dee1d18b388a945aa1bed91e94ce
Pratham <prathamsinghal2003@gmail.com>
2023-01-04 13:49:20 +0530

    :bug: Fixed Mongodb connect functionality

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    `mongodb+srv://root:${process.env.MONGO_ATLAS_PW}@cluster0.rdbmqot.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
